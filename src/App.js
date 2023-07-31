f89cc2c77dd54a58421dcc1a4c56fd3685baa355
Mahmoud Ashraf <66477803+m-Ash1@users.noreply.github.com>
2023-07-31 02:17:05 +0000

    make use of children prop

--- src/App.js
+++ src/App.js
@@ -38,23 +38,28 @@ const App = () => {
           <div className="message">{messages[steps - 1]}</div>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" color="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" color="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
   );
 };
+
+function Button({ color, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: color }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
 
 export default App;
